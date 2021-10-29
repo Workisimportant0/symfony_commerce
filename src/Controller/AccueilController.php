077f5860d4de722c67cde29b9e7df86d5891176b
Workisimportant0 <islam.ismailov2154@gmail.com>
2021-10-29 12:31:16 +0200

    modif

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Comments;
+use App\Entity\Produit;
 use App\Form\CommentsType;
 use App\Repository\ProduitRepository;
 use App\Repository\SlideRepository;
@@ -27,32 +28,49 @@ class AccueilController extends AbstractController
 
         $listeSlide = $repoSlide->findAll();
 
-        // Partie commentaires
-        // On crée le commentaire "vierge"
-
-        $comment = new Comments();
-
-        // On génére le formulaire
-        $commentForm = $this->createForm(CommentsType::class, $comment);
-
-        $commentForm->handleRequest($request);
-
-        // Traitement du formulaire
-        if($commentForm->isSubmitted() && $commentForm->isValid()){
-            $comment->setCreatedAt(new DateTimeImmutable());
-            $comment->setProduit($this->produit);
-
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($comment);
-            $em->flush();
-
-            $this->addFlash('message', 'Votre commentaire a bien été envoyé');
-            return $this->redirectToRoute('accueil', ['slug' => $this->produit->getSlug()]);
-        }
+        
 
         return $this->render('accueil/index.html.twig', ["listeProduits" => $listeProduits, "listeSlide" => $listeSlide, 
     ]);
 
         //render c'est une vue View 
     }
+
+    // #[Route('/plus-info/{id}', name: 'plus_info')]
+    // public function plusInfo(Produit $plusInfoProduit): Response
+    // {
+
+    //     // // Partie commentaires
+    //     // // On crée le commentaire "vierge"
+
+    //     // $comment = new Comments();
+
+    //     // // On génére le formulaire
+    //     // $commentForm = $this->createForm(CommentsType::class, $comment);
+
+    //     // $commentForm->handleRequest($request);
+
+    //     // // Traitement du formulaire
+    //     // if($commentForm->isSubmitted() && $commentForm->isValid()){
+    //     //     $comment->setCreatedAt(new DateTimeImmutable());
+    //     //     $comment->setProduit($this->produit);
+
+    //     //     $em = $this->getDoctrine()->getManager();
+    //     //     $em->persist($comment);
+    //     //     $em->flush();
+
+    //     //     $this->addFlash('message', 'Votre commentaire a bien été envoyé');
+    //     //     return $this->redirectToRoute('accueil', ['slug' => $this->produit->getSlug()]);
+    //     // }
+
+    //     return $this->redirectToRoute('plus_info', [':id' => $plusInfoProduit->getId()]);
+
+    //     return $this->render('plus-info/plusInfo.html.twig', [
+
+    //         'plusInfoProduit' => $plusInfoProduit,            
+
+    //     ]);
+
+    // }
+
 }
